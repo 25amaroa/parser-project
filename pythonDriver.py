ce851d79ad6465d12cb3c039423c8a47c2cd0056
25amaroa <anafcw@umsystem.edu>
2025-12-08 21:57:52 -0600

    here was that print tree function

--- pythonDriver.py
+++ pythonDriver.py
@@ -23,6 +23,32 @@ def printTree(node, parser, level=0):
 
         return result
 
+def drawTree(graph, root, parser):
+    stack = [(root, None)]   # (node, parentID)
+
+    while stack:
+        node, parentID = stack.pop()
+        nodeID = str(id(node))
+
+        # Create node label
+        if isinstance(node, TerminalNode):
+            label = f"'{node.getText()}'"
+        else:
+            ruleName = parser.ruleNames[node.getRuleIndex()]
+            label = ruleName
+
+        graph.node(nodeID, label=label)
+
+        # Connect to parentID
+        if parentID:
+            graph.edge(parentID, nodeID)
+
+        # you have to go in reverse to keep left to right 
+        for i in reversed(range(node.getChildCount())):
+            child = node.getChild(i)
+            stack.append((child, nodeID))
+
+
 
 file = "tests/project_deliverable_3.py"
 with open(file, "r") as f:
